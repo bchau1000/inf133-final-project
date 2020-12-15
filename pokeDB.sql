ef5aa3c1869a03fd6950bcd0b99596a6da94ac82
Boon Chai <booniiz82@gmail.com>
2020-12-14 22:28:58 -0800

    mergefix

--- pokeDB.sql
+++ pokeDB.sql
@@ -20,7 +20,7 @@ CREATE DATABASE IF NOT EXISTS pokedb;
 -- Table structure for table `pokemon`
 --
 
-
+USE pokedb;
 DROP TABLE IF EXISTS `pokemon`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
